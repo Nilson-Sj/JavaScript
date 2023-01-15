0a37c2eed40ff6abb193a93211b71e43c678a14d
Nilson Junior <108434705+Nilson-Sj@users.noreply.github.com>
2023-01-15 19:12:58 -0300

    Learned about comparison operators

---
+++ twelfth_project_op_compar/js/scripts.js
@@ -0,0 +1,32 @@
+var idade = 15;
+var possuiCarro = 1;
+
+if (idade >= 18) {
+  console.log('O usuário pode fazer a carteira');
+}
+
+if (idade <= 17) {
+  console.log('O usuário não pode fazer a carteira');
+}
+
+if (possuiCarro) {
+  console.log('O usuário já pode andar de carro');
+}
+
+var nome = 'Nilson'
+
+if (nome == 'Nilson') {
+  console.log('O seu nome é Nilson');
+}
+
+if (nome != 'Nilson') {
+  console.log('O seu nome não é Nilson');
+}
+
+if (20 > 10) {
+  console.log('Passou!');
+}
+
+if (100 < 20) {
+  console.log('Passou 2!');
+}
