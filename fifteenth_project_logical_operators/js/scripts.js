f9fb99ee84295ae860ea83591b96ab92d2a2ab58
Nilson-Sj <nilsonjr180795@gmail.com>
2023-01-16 10:44:37 -0300

    Learned about logical operators

---
+++ fifteenth_project_logical_operators/js/scripts.js
@@ -0,0 +1,16 @@
+var idade = 16;
+var nome = 'Nilson';
+
+if(nome == 'Nilson' && idade == 16){
+  console.log('O Nilson pode entrar na aula de programação!');
+} else {
+  console.log('Este não é o Nilson');
+}
+if(1 == 1 && 3 > 2 && true){
+  console.log('Passou!');
+}
+if((1 == 1 && 3 > 3) && true){
+  console.log('Passou!');
+} else if (nome === 'Nilson' && idade >= 14) {
+  console.log('Aqui Passa!');
+}
