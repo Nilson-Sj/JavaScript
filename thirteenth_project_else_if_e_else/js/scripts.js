a7245567c40fe363337ec9e48f9e87b4487e8a74
Nilson-Sj <nilsonjr180795@gmail.com>
2023-01-16 09:31:37 -0300

    Learned about conditional else if and else

---
+++ thirteenth_project_else_if_e_else/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = 'Nilson';
+
+if (nome == 'Pedro') {
+  console.log('O nome dele é Pedro');
+} else if(nome == 'Nilson') {
+  console.log('O nome é Nilson')
+} else if(nome == 'Rafael') {
+  console.log('O nome é Rafael');
+} else {
+  console.log('Ele possui outro nome!');
+}
+
+var idade = 19;
+
+if(idade > 20) {
+  console.log('Ele pode entrar na festa!');
+} else if(idade >= 18) {
+  console.log('Ele só pode entrar com autorização');
+}
+
+if(nome == 'Nilson') {
+  console.log('Teste');
+} else {
+  console.log('Testando');
+}
