84e93e6d867a9a92309c020a8a13220cccb63d03
Nilson-Sj <nilsonjr180795@gmail.com>
2023-01-17 12:32:04 -0300

    Learned about logical operators or ||

---
+++ 16_project_logical_operators_or/js/scripts.js
@@ -0,0 +1,25 @@
+/* 
+- O operador lógico || é conhecido com OR;
+- Ele retorna true caso uma das operações retorne verdadeiro;
+- O OR retorna false apenas se as duas expressões são falsas;
+*/
+var idade = 16;
+var nome = 'Nilson';
+
+if(nome == 'Nilson' || idade > 14) {
+  console.log('Pode entrar na aula de programação');
+} else {
+  console.log('Não pode entrar');
+}
+
+if(nome == 'Pedro' && (30 > 20 || 10 == 10)) {
+  console.log('Testando');
+} else {
+  console.log('Não Entrou');
+}
+
+if (nome == 'Pedro' || (30 > 20 && 10 == 10)) {
+  console.log('Testando');
+} else {
+  console.log('Não Entrou');
+}
